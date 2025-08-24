75873a230406078e1f5ecdfea95ee606e145d18f
Artem <tem4ik20.1@gmail.com>
2025-08-24 12:54:48 +0300

    fix swiper

--- src/js/key.js
+++ src/js/key.js
@@ -1,18 +1,26 @@
 import Swiper from 'swiper';
-import { Navigation, Mousewheel } from 'swiper/modules';
+import { Navigation, Mousewheel, EffectCoverflow } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/effect-coverflow';
 
 document.addEventListener("DOMContentLoaded", () => {
   const keySwiper = new Swiper(".swiper", {
-    modules: [Navigation, Mousewheel],
+    modules: [Navigation, Mousewheel, EffectCoverflow],
     direction: "vertical",
     slidesPerView: 3,
-    spaceBetween: 14,
-    navigation: false, 
-      mousewheel: true,
-   
-
+    spaceBetween: 20,
+    // centeredSlides: true, 
+    mousewheel: true,
+    // loop: true, 
+    // effect: 'coverflow', // дає ефект «розмитих» країв
+    // coverflowEffect: {
+    //   rotate: 0,      // поворот слайдів
+    //   stretch: 0,     // відстань між слайдами
+    //   depth: 100,     // ефект глибини
+    //   modifier: 1,
+    //   slideShadows: false,
+    // },
 
     breakpoints: {
       320: {
@@ -21,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       1200: {
         slidesPerView: 3,
-        spaceBetween: 20, 
+        spaceBetween: 20,
       },
     },
   });
